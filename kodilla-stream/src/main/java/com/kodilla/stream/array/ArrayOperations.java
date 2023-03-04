77b07aad1765a28efce148869d4c279630b8adc3
Tomasz Fidrocki <tomaszfidrocki90@gmail.com>
2023-03-04 14:22:34 +0000

    7.4 ArrayOperationsPartOne

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,26 @@
+package com.kodilla.stream.array;
+
+import java.util.DoubleSummaryStatistics;
+import java.util.OptionalDouble;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+public interface ArrayOperations {
+    static double getAverage(int[] numbers) {
+
+             IntStream.range(0, numbers.length)
+                     .map(i->numbers[i])
+                                     .forEach(System.out::println);
+
+        Double averageNumber = IntStream.range(0, numbers.length)
+                .mapToDouble(i->numbers[i])
+
+                    .average()
+                .orElse(0);
+
+            return averageNumber;
+
+        }
+}
+
+
---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,39 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    void testGetAverage() {
+//Given
+
+
+        int newNumbers[]= {10,10,5,15,10,15,15,40,50,80,10,12,14,18,22,20,36,20,24,10};
+        double sum= Arrays.stream(newNumbers).sum();
+        double average= sum/20;
+
+        //When
+        double result = ArrayOperations.getAverage(newNumbers);
+        double expextResult = average;
+
+        //then
+        assertEquals(expextResult, result);
+        System.out.println("Show avreage after stream   --"+ Arrays.stream(newNumbers).average());
+
+}
+}
+
+
+
+
+
+
+
+
+
+
